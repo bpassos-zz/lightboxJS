4bcde6211109efc08b9b6f958acfeb3989c2a1c1
Gabriel Oliveira <gabriel.oliveira@booking.com>
2014-09-18 19:08:56 +0200

    Added basic functionality
    Creating the overlay element and cloning the image to display it inside

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 // Click event to open image
 
-// append div to html to create overlay
+window.onload = function() {
+  // prepare the overlay
+  var overlayElm = document.createElement('div');
+  var bodyElm    = document.querySelector('body');
+
+  overlayElm.classList.add('lightboxJS-overlay');
+  // add overlay do body
+  bodyElm.insertBefore(overlayElm, bodyElm.firstChild);
+
+  var img = document.querySelector('img');
+
+  img.addEventListener('click', function(event) {
+    var newImg = this.cloneNode(false);
+    overlayElm.appendChild(newImg);
+
+    overlayElm.classList.add('lightboxJS-overlay-show');
+  }, false);
+};
